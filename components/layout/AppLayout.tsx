6b7c06bd2851c8e7c004db2345d8acd39834b48b
Uwem Israel <iamuwem@gmail.com>
2022-09-10 23:23:17 +0100

    fixed navbar navigation errors on mobile

--- components/layout/AppLayout.tsx
+++ components/layout/AppLayout.tsx
@@ -2,28 +2,29 @@ import React, { FC, useEffect, useState } from "react";
 import NavItem from "./NavItem";
 import { Icon } from "@iconify/react";
 import Link from "next/link";
-import Header from "./Header";
+import NavSection from "./NavSection";
 import AppContext from "context/index";
 import { AnimatePresence, motion } from "framer-motion";
 
 const AppLayout: FC<{ children: JSX.Element }> = ({ children }) => {
   const [isLoading, setIsLoading] = useState(true);
   useEffect(() => {
+    alert;
     const timeOut = setTimeout(() => {
       setIsLoading(false);
     }, 2000);
   }, []);
 
   return (
-    <div className=" bg-[#202020] h-screen overflow-clip">
+    <div className=" bg-[#202020] h-full overflow-clip main">
       {isLoading ? (
-        <div className="w-full h-full flex flex-col justify-center gap-5 items-center">
+        <div className="w-full h-screen flex flex-col justify-center gap-5 items-center">
           <Icon icon="logos:microsoft-icon" className="text-8xl " />
           <Icon icon="line-md:loading-loop" className="text-6xl text-white" />
         </div>
       ) : (
         <>
-          <div className="p-5">
+          <div className="p-5 h-[68px]">
             <div className="flex items-center gap-5">
               <Icon icon="logos:microsoft-icon" className="text-2xl " />
               <p className="dark:text-white font-medium text-lg">
@@ -31,14 +32,16 @@ const AppLayout: FC<{ children: JSX.Element }> = ({ children }) => {
               </p>
             </div>
           </div>
-          <div className="flex-col-reverse sm:flex-row flex h-[calc(100vh-50px)]">
-            <Header />
+          <div className="flex-row flex h-[calc(100vh-68px)]">
+            <header className="fixed bottom-0 w-full bg-[#202020] sm:w-auto z-50 sm:relative">
+              <NavSection />
+            </header>
             <AppContext>
               <motion.div
                 initial={{ opacity: 0, x: 100 }}
                 animate={{ opacity: 1, x: 0 }}
                 transition={{ bounce: false }}
-                className="flex-1 bg-[#222222] rounded-tl-md overflow-clip h-full"
+                className="bg-[#222222] rounded-tl-md overflow-auto flex-1 pb-0"
               >
                 <AnimatePresence mode="wait" initial={false}>
                   {children}
---
+++ components/layout/NavSection.tsx
@@ -0,0 +1,44 @@
+import React from "react";
+import NavItem from "./NavItem";
+import { Icon } from "@iconify/react";
+import { useRouter } from "next/router";
+import Link from "next/link";
+import { useRef } from "react";
+function NavSection() {
+  const { asPath } = useRouter();
+  const currenNavIndex = useRef(1);
+  const isLinkActive = (e: string) => {
+    return asPath.split("/")[1] === e;
+  };
+  return (
+    <nav className="px-1 flex sm:flex-col justify-between h-full sm:py-5">
+      <Link href={"/"}>
+        <a>
+          <NavItem
+            icon={<Icon icon={"fluent:home-16-filled"} />}
+            isActive={isLinkActive("")}
+            label="Home"
+          />
+        </a>
+      </Link>
+      <div className="flex sm:flex-col">
+        <Link href={"library"}>
+          <a>
+            <NavItem
+              icon={<Icon icon={"icomoon-free:books"} />}
+              isActive={isLinkActive("library")}
+              label="Library"
+            />
+          </a>
+        </Link>
+        <NavItem
+          icon={<Icon icon={"carbon:help"} />}
+          isActive={false}
+          label="Help"
+        />
+      </div>
+    </nav>
+  );
+}
+
+export default NavSection;
